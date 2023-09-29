1dcec8dbf4b93e26870021f5e156a14723152766
Puneet <puneet12345v@example.com>
2023-09-29 10:45:06 -0400

    Caurosal is made

---
+++ pazcare-app/components/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CarouselComponent from './slider/CarouselComponent'
+
+function Index() {
+  return (
+    <div>
+        <section>
+            <CarouselComponent></CarouselComponent>
+        </section>
+    </div>
+  )
+}
+
+export default Index
